55e915987a484537aea0decc7fbc24723a8769e6
Jacob Larsen <captainpowerturtle@gmail.com>
2019-02-27 12:25:32 +0100

    Fixed nogle ting for dig lasse

--- BilletLibrary/BaseClass.cs
+++ BilletLibrary/BaseClass.cs
@@ -13,27 +13,36 @@ namespace BilletLibrary
         /// </summary>
         /// <param name="nummerPlade"></param>
         /// <param name="dato"></param>
-        public BaseClass(string nummerPlade, DateTime dato)
+        public BaseClass(string nummerplade, DateTime dato)
         {
-            _nummerPlade = nummerPlade;
-            _dato = dato;
+            Nummerplade = nummerplade;
+            Dato = dato;
             broBizz = true;
-            if (nummerPlade.Length > 7)
-            {
-                throw new ArgumentException("Nummerplade er for lang!!");
-            }
         }
+        /// <summary>
+        /// En metode som retunere nummerpladen
+        /// </summary>
         public string Nummerplade
         {
             get { return _nummerPlade; }
-            set { _nummerPlade = value; }
+            set
+            {
+                if (value.Length > 7)
+                    throw new ArgumentException("Nummerplade kan ikke være længere end 7 tegn.");
+                _nummerPlade = value;
+            }
         }
         /// <summary>
         /// Abstract metoder der bliver overskrevet i henholdsvis bil og mc.
         /// </summary>
         /// <returns></returns>
         public abstract string Type();
+        /// <summary>
+        /// Abstract metode som repræsentere pris, som skal overskrives.
+        /// </summary>
         public abstract double Pris();
+
+        public DateTime Dato { get; set; }
 
 
     }
